1643139147f24079495bbb27c78fe7f8d19e2fc3
kilhyeon <tizona@naver.com>
2021-02-26 12:49:29 +0900

    URL 예제

---
+++ src/URLConnectionEx.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class URLConnectionEx {
+
+	public static void main(String[] args) throws MalformedURLException {
+		URL url = new URL("https://sports.news.naver.com/news.nhn?oid=425&aid=0000115723");
+
+		try (BufferedReader br = new BufferedReader(
+				new InputStreamReader(url.openStream()));
+				
+				BufferedWriter bw = new BufferedWriter(
+						new OutputStreamWriter(
+								new FileOutputStream("test.html")))) {
+			String line = null;
+
+			while ((line = br.readLine()) != null) {
+				System.out.println(line);
+				bw.write(line);
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
