fffc19b1d0773b1098630c3843d90f1970fa5832
kilhyeon <tizona@naver.com>
2021-03-08 09:32:36 +0900

    최종연산 - foreach, foreachorder, toArray

--- src/java_chobo2/ch14/stream/StreamForEachToArrayEx.java
+++ src/java_chobo2/ch14/stream/StreamForEachToArrayEx.java
@@ -1,7 +1,9 @@
 package java_chobo2.ch14.stream;
 
+import java.util.Arrays;
 import java.util.function.IntConsumer;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class StreamForEachToArrayEx {
 
@@ -20,7 +22,19 @@ public class StreamForEachToArrayEx {
 		IntStream.range(1, 10).parallel().forEachOrdered(System.out::print);
 		System.out.println();
 		
+		Student[] stdArr = {new Student("김인환", 1, 280), 
+				new Student("이태훈", 1, 270),
+				new Student("김상건", 2, 260),
+				new Student("전수린", 2, 250),
+				};
+		Stream<Student> studentStream = Arrays.stream(stdArr);
 		
+		Student[] tempArr = studentStream.toArray(Student[]::new);
+		System.out.println(Arrays.toString(tempArr));
+		
+		studentStream = Arrays.stream(stdArr);
+		Object[] objArr = studentStream.toArray();
+		System.out.println(Arrays.toString(objArr));
 	}
 
 }
